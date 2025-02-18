f45426112ef70a4dbad30d1bcfa70690a9b5fe88
bixson <alexanderornb@gmail.com>
2025-02-18 15:52:05 +0100

    added logic to methods in TouristService.java

--- src/main/java/services/TouristService.java
+++ src/main/java/services/TouristService.java
@@ -6,19 +6,28 @@ import repositories.TouristRepository;
 import java.util.List;
 
 public class TouristService {
+    private final TouristRepository touristRepository;
+
+    public TouristService(){
+        this.touristRepository = new TouristRepository();
+    }
     public List<TouristAttraction> getAllTouristAttractions() {
-        return TouristRepository.;
+        return touristRepository.getAllTouristAttractions();
     }
 
     public TouristAttraction getTouristAttraction(String name) {
+        return touristRepository.getTouristAttraction(name);
     }
 
     public void addTouristAttraction(TouristAttraction touristAttraction) {
+        touristRepository.addTouristAttraction(touristAttraction);
     }
 
     public void updateTouristAttraction(String name, TouristAttraction touristAttraction) {
+        touristRepository.updateTouristAttraction(name, touristAttraction);
     }
 
     public void deleteTouristAttraction(String name) {
+        touristRepository.deleteTouristAttraction(name);
     }
 }
